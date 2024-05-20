5087112de1be88beef4c5c32403b63a2022299e4
Kaan C. Fidan <kc.fidan@vispera.co>
2024-05-20 12:08:42 +0300

    Quality of life changes
    Consistent logo speed.
    Pastel colors.
    TM removed from logo.

--- main.lua
+++ main.lua
@@ -1,24 +1,30 @@
-function love.load() 
+require "color"
+
+
+function love.load()
     logo = {}
 
     logo.sprite = love.graphics.newImage("assets/logo.png")
-    logo.scale = 0.1
+    logo.scale = 0.3
     logo.width = logo.sprite:getWidth() * logo.scale
     logo.height = logo.sprite:getHeight() * logo.scale
 
     logo.color = {}
     logo.pos = {}
     logo.vel = {}
-    logo.speed = 10
+    logo.speed = 300
 
-    love.window.setMode(0, 0, {fullscreen = true, fullscreentype = "desktop"})
+    love.window.setMode(0, 0, {
+        fullscreen = true,
+        fullscreentype = "desktop"
+    })
 
     reset()
 end
 
 function reset()
     math.randomseed(os.time())
-    
+
     logo.color = choose_new_color()
     logo.pos = choose_random_pos()
 
@@ -32,7 +38,7 @@ function love.draw()
 end
 
 function love.update(dt)
-    update_pos()
+    update_pos(dt)
     process_border_hit()
 end
 
@@ -45,13 +51,13 @@ end
 function love.keypressed(key)
     if key == "r" then
         logo.vel.x = 0
-        logo.vel.y = 0     
+        logo.vel.y = 0
     end
 end
 
-function update_pos()
-    logo.pos.x = logo.pos.x + (logo.vel.x * logo.speed)
-    logo.pos.y = logo.pos.y + (logo.vel.y * logo.speed)
+function update_pos(dt)
+    logo.pos.x = logo.pos.x + (logo.vel.x * logo.speed * dt)
+    logo.pos.y = logo.pos.y + (logo.vel.y * logo.speed * dt)
 end
 
 function process_border_hit(w, h)
@@ -64,13 +70,23 @@ function process_border_hit(w, h)
 
     local hit = false
 
-    if top <= 0 or bottom >= h then
-        logo.vel.y = logo.vel.y * -1
+    if top <= 0 and logo.vel.y == -1 then
+        logo.vel.y = 1
         hit = true
     end
 
-    if left <= 0 or right >= w then
-        logo.vel.x = logo.vel.x * -1
+    if bottom >= h and logo.vel.y == 1 then
+        logo.vel.y = -1
+        hit = true
+    end
+
+    if left <= 0 and logo.vel.x == -1 then
+        logo.vel.x = 1
+        hit = true
+    end
+
+    if right >= w and logo.vel.x == 1 then
+        logo.vel.x = -1
         hit = true
     end
 
@@ -79,13 +95,18 @@ function process_border_hit(w, h)
     end
 end
 
-function choose_new_color() 
-    local c
+function choose_new_color()
+    local h, s, v, a = rgb_hsv(logo.color.R, logo.color.G, logo.color.B, logo.color.A) 
 
-    repeat c = {R = random_binary(), G = random_binary(), B = random_binary(), A = 1}
-    until (c.R ~= logo.color.R or c.G ~= logo.color.G or c.B ~= logo.color.B) and not (c.R == c.G and c.R == c.B)
+    if h ~= nil then
+        h = (h + math.random() * 0.2 + 0.5) % 1 -- choose a color from the other side of the wheel with random perturbance
+    else
+        h = math.random()
+    end
 
-    return c
+    local r, g, b, a = hsv_rgb(h, 0.3, 1, 1)
+
+    return { R = r, G = g, B = b, A = a }
 end
 
 function choose_random_pos()
@@ -93,7 +114,11 @@ function choose_random_pos()
 
     local pos
 
-    repeat pos = {x = math.random() * w, y = math.random() * h}
+    repeat
+        pos = {
+            x = math.random() * w,
+            y = math.random() * h
+        }
     until (pos.x + logo.width < w and pos.y + logo.height < h)
 
     return pos
